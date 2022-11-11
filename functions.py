17709bfccf059f303331d9527a2bffdae2f3a291
Fjeldstad <vfjeldstad@deloitte.no>
2022-11-11 13:11:13 +0100

    v1 finish

--- functions.py
+++ functions.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 from users import User
+from stock import Stock
 
-username = 'vilde'
-users = {}
 
 def create_user(username):
     user_balance = float(input('Enter your balance: '))
     user = User(username, user_balance)
-    users[user.user_name] = user
-    
-    #user = User(username, user_balance)
-    #print(user.user_name, user.balance)
+    return user
 
-user = create_user(username)
+#Oppdatere brukerens portefølje
+def buy(user, user_stock_name, stock_price, stock_purchase_date, user_stock_quantity):
+    user.portfolio[user_stock_name] = {}
+    user.portfolio[user_stock_name]['Purchase_date'] = stock_purchase_date
+    user.portfolio[user_stock_name]['Purchase_price'] = stock_price
+    user.portfolio[user_stock_name]['Purchase_quantity'] = user_stock_quantity
+    user.portfolio[user_stock_name]['Cost_price'] = stock_price * user_stock_quantity
+    
+    #Oppdater brukerens balanse
+    user.balance -= stock_price * user_stock_quantity
+    
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Created on Thu Nov 10 11:56:39 2022
 from datapull import datapull
 from users import User
 from stock import Stock
+from functions import *
 #from functions 
 
 #df = datapull()
@@ -18,31 +19,34 @@ if __name__ == '__main__':
 #1) En bruker opprettes basert på tekstinnput
     while True:
         username = input('Enter the username: ')
-        user_balance = float(input('Enter your balance: '))
-        user = User(username, user_balance)
+        #Sjekker om bruker finnes fra før. Hvis burker finnes hentes info fra bruker, hvis ikke opprettes ny bruker
+        if username in users.keys():
+            user = users[username]
+        else:
+            user = create_user(username)
+            users[user.user_name] = user
         print(user.user_name, user.balance)
 
 #2) Brukeren får tilbudet om å kjøpe en valgfri aksje og deretter antall av aksjen
         #Initier aksjen
         user_stock_name = input('Enter stock name: ')
+        action = input('Choose buy or sell: ')
         user_stock_quantity = int(input('Enter stock quantity: '))
         stock = Stock(user_stock_name)
-
+        
 #3) Aksjedata hentes fra API (siste tilgjengelige sluttpris og dato), og brukerens portefølje(kostpris, aksjenavn, kjøpsdato og antall askjer) og balanse oppdateres
         #Hent aksjepris
         stock_price = float(stock.get_lastest_stock_price())
         stock_purchase_date = stock.get_stock_purchasedate()
         
-        #Oppdatere brukerens portefølje
-        user.portfolio[user_stock_name] = {}
-        user.portfolio[user_stock_name]['Purchase_date'] = stock_purchase_date
-        user.portfolio[user_stock_name]['Purchase_price'] = stock_price
-        user.portfolio[user_stock_name]['Purchase_quantity'] = user_stock_quantity
-        user.portfolio[user_stock_name]['Cost_price'] = stock_price * user_stock_quantity
-        
-        #Oppdater brukerens balanse
-        user.balance -= stock_price * user_stock_quantity
+        if action == 'buy':
+            buy(user, user_stock_name, stock_price, stock_purchase_date, user_stock_quantity)
+        else:
+            continue
+
         
         print(user.balance)
         
+        #Oppdater bruker i databasen (users)
+        users[user.user_name] = user
 #4) Print brukeren og brukerens portefølje og balanse
